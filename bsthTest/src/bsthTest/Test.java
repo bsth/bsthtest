ff14b0e6ebb2e551538c851e44c37c70732a387b
zq <123456789>
2016-06-30 16:02:44 +0800

    test

--- bsthTest/src/bsthTest/Test.java
+++ bsthTest/src/bsthTest/Test.java
@@ -3,7 +3,7 @@ package bsthTest;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(12);
-		System.out.println(123);
+		System.out.println(234);
 		System.out.println(12);
 		System.out.println(12);
 	}
