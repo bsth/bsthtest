308db3ff3f26de94c2da5a4dc5688f17b8aa4783
zq <123456789>
2016-07-01 09:40:21 +0800

    gtet

--- bsthTest/src/bsthTest/Test.java
+++ bsthTest/src/bsthTest/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println(12);
 		System.out.println("hello");
-		System.out.println(12);
+		System.out.println(13);
 		System.out.println(12);
 		System.out.println(12);
 	}
--- bsthTest/src/bsthTest/Test001.java
+++ bsthTest/src/bsthTest/Test001.java
@@ -2,7 +2,7 @@ package bsthTest;
 
 public class Test001 {
 	public static void main(String[] args) {
-		System.out.println(12);
+		System.out.println(13);
 		System.out.println("hello");
 		System.out.println("world");
 	}
