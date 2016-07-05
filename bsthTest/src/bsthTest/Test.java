2f822c171c3a03d2d2600c54ce541c97449ba0c9
20140103 <20140103@20140103-106cbf>
2016-07-05 12:46:45 +0800

    fxy

--- bsthTest/src/bsthTest/Test.java
+++ bsthTest/src/bsthTest/Test.java
@@ -3,5 +3,6 @@ package bsthTest;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("zq");
+		System.out.println("fxy");
 	}
 }
--- bsthTest/src/bsthTest/Test001.java
+++ bsthTest/src/bsthTest/Test001.java
@@ -3,5 +3,6 @@ package bsthTest;
 public class Test001 {
 	public static void main(String[] args) {
 		System.out.println("zq");
+		System.out.println("fxy");
 	}
 }
