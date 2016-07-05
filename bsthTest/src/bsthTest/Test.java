e5dc82ea1679f590acc5395f6eb91f0fd58d8dd1
zq <123456789>
2016-07-05 12:38:26 +0800

    t

--- bsthTest/src/bsthTest/Test.java
+++ bsthTest/src/bsthTest/Test.java
@@ -2,17 +2,6 @@ package bsthTest;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(12);
-		System.out.println("hello");
-		System.out.println(13);
-		System.out.println(12);
-		System.out.println(12);
-		System.out.println("world");
-		System.out.println("world222222222222");
-		System.out.println("world2");
-
-		System.out.println("world33");
-
-		System.out.println("333333333333");
+	
 	}
 }
--- bsthTest/src/bsthTest/Test001.java
+++ bsthTest/src/bsthTest/Test001.java
@@ -2,12 +2,6 @@ package bsthTest;
 
 public class Test001 {
 	public static void main(String[] args) {
-		System.out.println(13);
-		System.out.println("hello");
-		System.out.println("world");
-		System.out.println("world222222222222222");
-		System.out.println("world2");
 		
-		System.out.println("world33");
 	}
 }
